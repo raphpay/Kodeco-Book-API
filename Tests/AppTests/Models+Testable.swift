9a2a5371cd7e553ce098336af8aa11aa25d18fab
raphpay <raphpay.dev@gmail.com>
2024-01-23 16:09:00 +0100

    Testing user

--- Tests/AppTests/Models+Testable.swift
+++ Tests/AppTests/Models+Testable.swift
@@ -18,3 +18,26 @@ extension User {
         return user
     }
 }
+
+enum CreationError: Error {
+    case badUserID
+}
+
+extension Acronym {
+    static func create(short: String = "OMG",
+                       long: String = "Oh my god",
+                       user: User? = nil,
+                       on database: Database) async throws -> Acronym {
+        var acronymsUser = user
+        if acronymsUser == nil {
+            acronymsUser = try await User.create(on: database)
+        }
+        
+        guard let userID = acronymsUser?.id else { throw CreationError.badUserID }
+        
+        let acronym = Acronym(short: short, long: long, userID: userID)
+        try await acronym.save(on: database)
+        
+        return acronym
+    }
+}
--- Tests/AppTests/UserTests.swift
+++ Tests/AppTests/UserTests.swift
@@ -11,6 +11,8 @@ import XCTVapor
 final class UserTests: XCTestCase {
     let expectedName = "Alice"
     let expectedUsername = "alicemerveilles"
+    let expectedShort = "WTF"
+    let expectedLong = "What The Flip"
     let usersURI = "/api/users/"
     var app: Application!
     
@@ -25,13 +27,31 @@ final class UserTests: XCTestCase {
         super.tearDown()
     }
     
+    func testUserCanBeCreatedOnAPI() async throws {
+        app = try await Application.testable()
+        
+        let user = User(name: expectedName, username: expectedUsername)
+        
+        try app.test(.POST, usersURI) { req in
+            try req.content.encode(user)
+        } afterResponse: { response in
+            XCTAssertEqual(response.status, .ok)
+            
+            let receivedUser = try response.content.decode(User.self)
+            XCTAssertEqual(receivedUser.name, expectedName)
+            XCTAssertEqual(receivedUser.username, expectedUsername)
+            XCTAssertNotNil(receivedUser.id)
+        }
+
+    }
+    
     func testUsersCanBeRetrievedFromAPI() async throws {
         app = try await Application.testable()
         
         let user = try await User.create(name: expectedName, username: expectedUsername, on: app.db)
         let _ = try await User.create(on: app.db)
         
-        try app.test(.GET, "/api/users") { response in
+        try app.test(.GET, usersURI) { response in
             XCTAssertEqual(response.status, .ok)
             
             let users = try response.content.decode([User].self)
@@ -40,6 +60,47 @@ final class UserTests: XCTestCase {
             XCTAssertEqual(users[0].name, expectedName)
             XCTAssertEqual(users[0].username, expectedUsername)
             XCTAssertEqual(users[0].id, user.id)
+        }
+    }
+    
+    func testGetSingleUserFromAPI() async throws {
+        app = try await Application.testable()
+        
+        let user = try await User.create(name: expectedName, username: expectedUsername, on: app.db)
+        let _ = try await User.create(on: app.db)
+        
+        guard let userID = user.id else { return }
+        
+        try app.test(.GET, "\(usersURI)/\(userID)") { response in
+            XCTAssertEqual(response.status, .ok)
+            
+            let receivedUser = try response.content.decode(User.self)
+            
+            XCTAssertEqual(receivedUser.name, expectedName)
+            XCTAssertEqual(receivedUser.username, expectedUsername)
+            XCTAssertEqual(receivedUser.id, userID)
+        }
+    }
+    
+    func testGetUsersAcronyms() async throws {
+        app = try await Application.testable()
+        
+        let user = try await User.create(on: app.db)
+        let acronym = try await Acronym.create(short: expectedShort, long: expectedLong, user: user, on: app.db)
+        let _ = try await Acronym.create(short: "LOL", long: "Laug Out Loud", user: user, on: app.db)
+        
+        guard let userID = user.id else { return }
+        
+        try app.test(.GET, "\(usersURI)/\(userID)/acronyms") { response in
+            XCTAssertEqual(response.status, .ok)
+            
+            let acronyms = try response.content.decode([Acronym].self)
+            
+            XCTAssertEqual(acronyms.count, 2)
+            XCTAssertEqual(acronyms[0].short, expectedShort)
+            XCTAssertEqual(acronyms[0].long, expectedLong)
+            XCTAssertEqual(acronyms[0].id, acronym.id)
+            XCTAssertEqual(acronyms[0].user.id, userID)
         }
     }
 }
